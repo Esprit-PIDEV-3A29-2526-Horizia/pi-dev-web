d33240c708f8ec5c02a93eb5c8f79e59a836ff24
linarouabeh <lina.rouabeh@esprit.tn>
2026-04-04 03:37:04 +0100

    almost full events cruds

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,13 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
-        return $this->render('admin/dashboard.html.twig');
+        return $this->redirectToRoute('app_event_index');
     }
 }
